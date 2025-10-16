5cd475a7e6ad1753feaf621ebe4396d398d3193f
nborges <nick.kras.borges@fazenda.rj.gov.br>
2025-10-16 17:07:08 -0300

    new commit

---
+++ src/main/java/com/example/ms_boot/MsBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootApplication.class, args);
+	}
+
+}
