0d8cd9c847c57b66a95f8494f2f228234bd2843e
nborges <nick.kras.borges@fazenda.rj.gov.br>
2025-10-16 17:07:08 -0300

    add spring boot

---
+++ src/test/java/com/example/ms_boot/MsBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
